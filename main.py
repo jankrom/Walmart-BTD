3f36abb7a02aead3568ecd6122416bc2ab54e0df
jankrom <justinankrom1@gmail.com>
2021-11-12 19:07:50 -0500

    Circles / game over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from cmu_112_graphics import *
 import math
+import random
 
 '''
 Content:
@@ -33,43 +34,66 @@ Ideas:
     # radius/size
     # damage it causes at the end
 class Enemy(object):
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     
+    def drawEnemy(self, canvas, app):
+        if len(app.enemies) >= 1:
+            canvas.create_oval(self.x-self.r, self.y-self.r, self.x+self.r, 
+                           self.y+self.r, fill = self.color, width = 2)
+    
+    def moveEnemy(self, app):
+        self.x += self.speed
+        self.y += 0
+        if (self.x-self.r >= app.width or self.x+self.r <= 0 or 
+            self.y-self.r >= app.height or self.y+self.r <= 0):
+            app.health -= self.destruct
+            app.enemies.remove(self)
 
-class Bat(Enemy):
-    def __init__(self):
-        url = "https://i.pinimg.com/originals/05/98/df/0598dfb167a45c5ffdeef6ff6f644102.png"
-        self.img = self.loadImage(url)
-        self.speed = 20
+class Red(Enemy):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 4
         self.camo = False
         self.health = 100
-        self.radius = 20
+        self.r = 20
         self.destruct = 2
+        self.color = 'red'
 
-class Skeleton(Enemy):
-    def __init__(self):
-        self.speed = 10
+class Blue(Enemy):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 4
         self.camo = False
         self.health = 200
-        self.radius = 20
+        self.r = 20
         self.destruct = 5
+        self.color = 'blue'
 
-class Spider(Enemy):
-    def __init__(self):
-        self.speed = 40
+class Yellow(Enemy):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 7
         self.camo = True
         self.health = 150
-        self.radius = 20
+        self.r = 20
         self.destruct = 3
+        self.color = 'yellow'
 
 class Boss(Enemy):
-    def __init__(self):
-        self.speed = 50
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 2
         self.camo = False
         self.health = 1000
-        self.radius = 50
+        self.r = 50
         self.destruct = 20
+        self.color = 'brown'
 
 
 #############################################################################
@@ -120,30 +144,69 @@ def appStarted(app):
     app.height = 600
     app.width = 800
     app.enemies = [ ] 
+    #app.enemyList [ Red, Blue, Yellow, Boss ]
     app.turrets = [ ]
+    app.wave = 1
+    app.health = 100
+    app.pause = True
+    app.gameOver = False
+
+def spawnEnemy(app):
+    enemyList = [ Red, Blue, Yellow, Boss ]
+    randEnemy = random.randint(0, len(enemyList) - 1)
+    app.enemies.append(enemyList[randEnemy](0, 300))
+    if len(app.enemies) >= 1:
+        for enemy in app.enemies:
+            enemy.moveEnemy(app)
 
 def timerFired(app):
-    pass
+    if app.health <= 0 :
+        app.gameOver = True
+        app.pause = False
+    if app.pause == True:
+        spawnEnemy(app)
+
 
 def keyPressed(app, event):
-    pass
+    if event.key == 'Escape':
+        app.pause = not app.pause
+
 
 def mousePressed(app, event):
-    pass
+    # using this to debug as i can spawn wherever I want
 
-def drawEnemy(app, canvas):
+    x = event.x
+    y = event.y
+    enemyList = [ Red, Blue, Yellow, Boss ]
+    randEnemy = random.randint(0, len(enemyList) - 1)
+    app.enemies.append(enemyList[randEnemy](x, y))
+
     pass
 
 def drawTurret(app, canvas):
     pass
 
+def drawGameOver(app, canvas):
+    canvas.create_text(app.width // 2, app.height // 2, text = "GAME OVER",
+                        font = '100')
+
+def drawHealth(app, canvas):
+    margin = 50
+    canvas.create_text(app.width // 2, margin, 
+                    text = f"Health: {app.health}", font = "Arial 26 bold")
+
+
 def redrawAll(app, canvas):
-    canvas.create_rectangle(0,0, app.width, app.height, fill = "black")
-    drawEnemy(app, canvas)
-    drawTurret(app, canvas)
+    canvas.create_rectangle(0,0, app.width, app.height, fill = "green")
+    if app.health <= 0:
+        drawGameOver(app, canvas)
+    drawHealth(app, canvas)
+    if len(app.enemies) >= 1:
+        for enemy in app.enemies:
+            enemy.drawEnemy(canvas, app)
+    #drawTurret(app, canvas)
 
 
-
-runApp(width = 800, height = 600)
+runApp(width = 600, height = 600)
 
         
