3b55ea92b58c6711202f425b9060dd06ea34f136
jankrom <justinankrom1@gmail.com>
2021-11-16 01:11:33 -0500

    shop + hardcoded path

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ Ideas:
     Gets progressively harder as the rounds go by
 '''
 
+
 #############################################################################
 # Splash Screen 
 #############################################################################   
@@ -38,6 +39,7 @@ class Enemy(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.seenPath = set()
     
     def drawEnemy(self, canvas, app):
         margin = 10
@@ -47,12 +49,41 @@ class Enemy(object):
             canvas.create_text(self.x, self.y - margin, text = f"{self.health}")
     
     def moveEnemy(self, app):
-        self.x += self.speed
-        self.y += 0
         if (self.x-self.r >= app.width or self.x+self.r <= 0 or 
             self.y-self.r >= app.height or self.y+self.r <= 0):
             app.health -= self.destruct
             app.enemies.remove(self)
+        else:
+            (row, col) = getCell(app, self.x, self.y)
+            (x0, y0, x1, y1) = getCellBounds(app, row, col) 
+            self.seenPath.add((row, col))
+            if moveEnemyLegalX(self, app, row, col):
+                #if (self.y <= ((y0 + y1)//2) + 2) and (self.y >= ((y0 + y1)//2) - 2)
+                self.x += self.speed
+            elif moveEnemyLegalY(self, app, row, col):
+                #if (self.x <= (x0 + x1)//2 + 2) and self.x >= ((x0 + x1)//2) - 2:
+                self.y += self.speed
+
+# checks if a x direction move is legal
+def moveEnemyLegalX(self, app, row, col):
+    if (row, col+1) not in self.seenPath and app.board[row][col+1] == app.pathColor:
+        self.speed = abs(self.speed)
+        return True
+    elif (row, col-1) not in self.seenPath and app.board[row][col-1] == app.pathColor:
+        self.speed = -abs(self.speed)
+        return True
+    return False
+
+# checks if a y direction move is legal
+def moveEnemyLegalY(self, app, row, col):
+    if (row+1, col) not in self.seenPath and app.board[row+1][col] == app.pathColor:
+        self.speed = abs(self.speed)
+        return True
+    elif (row-1, col) not in self.seenPath and app.board[row-1][col] == app.pathColor:
+        self.speed = -abs(self.speed)
+        return True
+    return False
+
 
 # basic balloon
 class Red(Enemy):
@@ -65,6 +96,7 @@ class Red(Enemy):
         self.r = 20
         self.destruct = 2
         self.color = 'red'
+        self.seenPath = set()
 
 # sane as red but with more HP
 class Blue(Enemy):
@@ -77,6 +109,7 @@ class Blue(Enemy):
         self.r = 20
         self.destruct = 5
         self.color = 'blue'
+        self.seenPath = set()
 
 # faster than blue and yellow, but with less HP
 class Yellow(Enemy):
@@ -89,6 +122,7 @@ class Yellow(Enemy):
         self.r = 20
         self.destruct = 3
         self.color = 'yellow'
+        self.seenPath = set()
 
 # much slower but has a lot more HP
 class Boss(Enemy):
@@ -101,6 +135,7 @@ class Boss(Enemy):
         self.r = 50
         self.destruct = 20
         self.color = 'brown'
+        self.seenPath = set()
 
 
 #############################################################################
@@ -156,7 +191,7 @@ class Bomber(Turret):
         self.x = x
         self.y = y
         self.range = 200
-        self.r = 30
+        self.r = 50
         self.attackSpeed = 5
         self.typeShot = True
         self.shotRadius = 100
@@ -168,20 +203,108 @@ class Bomber(Turret):
 # Drawing
 #############################################################################
 
+
 def appStarted(app):
+    app.rows = app.height // 20
+    app.cols = app.width // 40
+    app.cellWidth = (app.width-200) // app.cols
+    app.cellHeight = app.cellWidth
+    app.shopMargin = app.width // 4
+    app.boardColor = 'green'
+    app.pathColor = 'goldenrod3'
+    #app.board = [ [app.boardColor] * app.cols for row in range(app.rows)]
+    app.board = hardCodedPath(app)
     app.enemies = [ ] 
     app.turrets = [ ]
     app.wave = 1
     app.health = 100
-    app.money = 100
+    app.currency = 100
     app.pause = True
     app.gameOver = False
+
+# https://www.cs.cmu.edu/~112/notes/notes-animations-part2.html
+# return True if (x, y) is inside the grid defined by app.
+def pointInGrid(app, x, y):
+    return ((app.margin <= x <= app.width-app.margin) and
+            (app.margin <= y <= app.height-app.margin))
+
+# https://www.cs.cmu.edu/~112/notes/notes-animations-part2.html
+# return (row, col) in which (x, y) occurred or (-1, -1) if outside grid.
+def getCell(app, x, y):
+    row = int(y / app.cellHeight)
+    col = int(x / app.cellWidth)
+    return (row, col)
+
+# https://www.cs.cmu.edu/~112/notes/notes-animations-part2.html
+# returns (x0, y0, x1, y1) corners/bounding box of given cell in grid
+def getCellBounds(app, row, col):
+    x0 = col * app.cellWidth
+    x1 = (col+1) * app.cellWidth
+    y0 = row * app.cellHeight
+    y1 = (row+1) * app.cellHeight
+    return (x0, y0, x1, y1)
+
+# creates a hardcoded map
+def hardCodedPath(app):
+    board = [ [app.boardColor] * app.cols for row in range(app.rows)]
+    for row in range(app.rows):
+        for col in range(app.cols):
+            if row == 2 and col < 18:
+                board[row][col] = app.pathColor
+            elif col == 17 and row > 2 and row < 13:
+                board[row][col] = app.pathColor
+            elif row == 13 and col < 18 and col > 12:
+                board[row][col] = app.pathColor
+            elif col == 12 and row < 14 and row > 5:
+                board[row][col] = app.pathColor
+            elif row == 6 and col < 12 and col > 6:
+                board[row][col] = app.pathColor
+            elif col == 7 and row < 14 and row > 5:
+                board[row][col] = app.pathColor
+            elif row == 13 and col < 8:
+                board[row][col] = app.pathColor
+    return board
+
+# from HW 6 (Tetris)
+# draws the cells of the board using the given dimensions and color
+def drawCell(app, canvas, row, col, color):
+    canvas.create_rectangle(col*app.cellWidth, row*app.cellHeight, 
+    (col+1)*app.cellWidth,(row+1)*app.cellHeight,fill = color, outline = '')
+
+# from HW 6 (Tetris)
+# draws the board by drawing the cells using the drawCell function
+def drawBoard(app, canvas):
+    for row in range(app.rows):
+        for col in range(app.cols):
+            color = app.board[row][col]
+            drawCell(app, canvas, row, col, color)
+
+# draws the shop
+def drawShop(app, canvas):
+    w = app.width
+    h = app.height
+    m = app.shopMargin
+    canvas.create_rectangle(w-m, 0, w, h, fill = 'grey')
+    canvas.create_text(w - m//2, 75, text = "SHOP", font = "Arial 26 bold")
+    canvas.create_text(w - m//2, 100, text = f"Currency -- {app.currency}", font = "Arial 15 bold")
+    # creates cannon shop
+    canvas.create_rectangle(w-m//4, h//4, w-m*3//4, h//4+100, fill = 'white')
+    canvas.create_rectangle(w-m//4-20, h//4+20, w-m*3//4+20, h//4+100-20, fill = 'black')
+    canvas.create_text(w - m//2, h//4+110, text = "Cannon", font = "Arial 15 bold")
+    # creates darter shop
+    canvas.create_rectangle(w-m//4, h//4+150, w-m*3//4, h//4+250, fill = 'white')
+    canvas.create_rectangle(w-m//4-20, h//4+150+20, w-m*3//4+20, h//4+250-20, fill = 'red')
+    canvas.create_text(w - m//2, h//4+260, text = "Darter", font = "Arial 15 bold")
+    # creates bomb tower shop
+    canvas.create_rectangle(w-m//4, h//4+300, w-m*3//4, h//4+400, fill = 'white')
+    canvas.create_rectangle(w-m//4-20, h//4+300+20, w-m*3//4+20, h//4+400-20, fill = 'grey')
+    canvas.create_text(w - m//2, h//4+410, text = "Bomb Tower", font = "Arial 15 bold")
 
 # spawns enemies
 def spawnEnemy(app):
     enemyList = [ Red, Blue, Yellow, Boss ]
     randEnemy = random.randint(0, len(enemyList) - 1)
-    app.enemies.append(enemyList[randEnemy](0, 300))
+    app.enemies.append(enemyList[randEnemy](0, 75))
 
 # moves each enemy
 def moveEnemy(app):
@@ -275,15 +398,17 @@ def drawGameOver(app, canvas):
 
 #draws how much health player has left
 def drawHealth(app, canvas):
-    margin = 50
-    canvas.create_text(app.width // 2, margin, 
+    margin = 35
+    canvas.create_text((app.width - app.shopMargin) // 2, margin, 
                     text = f"Health: {app.health}", font = "Arial 26 bold")
 
 
 def redrawAll(app, canvas):
-    canvas.create_rectangle(0,0, app.width, app.height, fill = "green")
+    #canvas.create_rectangle(0,0, app.width, app.height, fill = "green")
     if app.health <= 0:
         drawGameOver(app, canvas)
+    drawBoard(app, canvas)
+    drawShop(app, canvas)
     drawHealth(app, canvas)
     if len(app.turrets) >= 1:
         for turret in app.turrets:
